3b74766cf6c144eecdfc8f547e6b8e05d78092d9
“Roni” <roniemail1@gmail.com>
2019-01-08 17:30:55 +0000

    Updates

---
+++ precache-manifest.fc9e38a22f03e11f9fa11c4785fbc8d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4757cdd45c48fa8bcf1b",
+    "url": "github.com/roni5/my-redux/static/js/runtime~main.4757cdd4.js"
+  },
+  {
+    "revision": "187c017631d35ae22808",
+    "url": "github.com/roni5/my-redux/static/js/main.187c0176.chunk.js"
+  },
+  {
+    "revision": "a046775c927a35582e9a",
+    "url": "github.com/roni5/my-redux/static/js/1.a046775c.chunk.js"
+  },
+  {
+    "revision": "187c017631d35ae22808",
+    "url": "github.com/roni5/my-redux/static/css/main.95779253.chunk.css"
+  },
+  {
+    "revision": "af83c8bb94e131ee6ed7aa0219b9e8f0",
+    "url": "github.com/roni5/my-redux/index.html"
+  }
+];
