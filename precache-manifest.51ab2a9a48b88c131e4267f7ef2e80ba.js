f4fa214408b4641335efb837851d4282370f5d80
“Roni” <roniemail1@gmail.com>
2019-01-08 20:19:43 +0000

    Updates

---
+++ precache-manifest.51ab2a9a48b88c131e4267f7ef2e80ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b62a5dd8ffc449edaf9",
+    "url": "/my-redux/static/js/runtime~main.0b62a5dd.js"
+  },
+  {
+    "revision": "81c42a8e540ae2e8ab73",
+    "url": "/my-redux/static/js/main.81c42a8e.chunk.js"
+  },
+  {
+    "revision": "0645851f156af39221bf",
+    "url": "/my-redux/static/js/1.0645851f.chunk.js"
+  },
+  {
+    "revision": "81c42a8e540ae2e8ab73",
+    "url": "/my-redux/static/css/main.e9d70eec.chunk.css"
+  },
+  {
+    "revision": "8afb40742d7c81477fe39f87d6800e47",
+    "url": "/my-redux/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-redux/precache-manifest.ead0212bd2943b45fc9bac58266be7b3.js"
+  "/my-redux/precache-manifest.51ab2a9a48b88c131e4267f7ef2e80ba.js"
 );
 
 workbox.clientsClaim();
